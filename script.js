62ce469a586a96733b58395d6f38d28444ca50a3
MANOJ-02460 <131439674+MANOJ-02460@users.noreply.github.com>
2026-06-21 14:24:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function makeid(l) {
   // write your code here
+	let chars = [];
+
+	for (let i = 97; i <= 122; i++) {
+	    chars.push(i)
+	}
+
+	let result = ""
+
+	for (let j = 1; j <= l; j++){
+	
+	    let randomCode = chars[Math.floor(Math.random() * chars.length)]
+	    let randomChar = String.fromCharCode(randomCode);
+	    result += randomChar
+	   
+	}
+
+	return result
 }
 
 // Do not change the code below.
